d8230cbf38441aed9cc8ca0fa5e6eaa56b1052dc
Meregalli Christian <meregalli.christian@osdbsesto>
2022-05-10 13:35:28 +0200

    remove loop

--- 3BIFirstJava/src/pkg3bifirstjava/Main.java
+++ 3BIFirstJava/src/pkg3bifirstjava/Main.java
@@ -16,9 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        for (int i=0;;i++){
-            System.out.println(i);
-        }
+        
     }
     
 }
